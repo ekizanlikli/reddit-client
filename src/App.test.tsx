682e7f2c661829a2eed88ede3e8fb420e3523cb5
ekizanlikli <ekizanlikli@gmail.com>
2023-03-13 21:31:20 +0100

    test-fail-trial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders ahmet link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/ahmet/i);
+  expect(linkElement).toBeInTheDocument();
+});
